5573b8d2ab1a592955e373f2bdcc9fd7707e451a
sergiotreiger <sergio.treiger@gmail.com>
2019-11-06 19:47:16 -0200

    sonarlint

--- src/main/java/com/myflyway/demo/FlywayMaven/App.java
+++ src/main/java/com/myflyway/demo/FlywayMaven/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! é o caralho" );
-        
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
     }
 }
